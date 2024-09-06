b7f5678acac58319117dca5362f714c6da1d796b
Jacob Grass <jacob@jacobgrass.com>
2024-09-06 11:23:01 -0600

    add neogen

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -12,6 +12,15 @@ local plugins = {
       ensure_installed = {
         "clangd"
       }
+    },
+    {
+      "danymat/neogen",
+      config = function()
+        require('neogen').setup( {})
+      end,
+      lazy = false
+      -- Uncomment next line if you want to follow only stable versions
+      -- tag = "*"
     }
   }
 
