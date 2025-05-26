81441c4a4e9934909ea8a87dea99f343cd010349
Jacob Grass <jacob.grass.59@astradian.com>
2025-05-25 18:49:02 -0600

    add csharp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,6 +4,12 @@ local on_attach = base.on_attach
 local capabilities = base.capabilities
 
 local lspconfig = require("lspconfig")
+local util = require("lspconfig.util") -- For root_dir patterns
+
+-- Common root directory function for .NET projects
+local csharp_root_dir = function(fname)
+  return util.root_pattern("*.sln", "*.csproj", ".git")(fname)
+end
 
 -- Existing clangd setup
 lspconfig.clangd.setup {
@@ -77,25 +83,59 @@ lspconfig.marksman.setup {
   capabilities = capabilities,
 }
 
--- lspconfig.mdformat.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
--- }
-
 -- Fortran
 lspconfig.fortls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
-lspconfig.docker_compose_language_service.setup{
+lspconfig.docker_compose_language_service.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "docker-compose.yml" },
 }
 
-lspconfig.dockerls.setup{
+lspconfig.dockerls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = { "dockerfile" },
+}
+
+-- 󰄳 C# / OmniSharp
+lspconfig.omnisharp.setup {
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr) -- Call your base on_attach
+
+    -- OmniSharp's formatting capabilities will be used by default.
+    -- No need to disable them if you are not using a separate formatter like csharpier.
+
+    -- You can add other C# specific keymaps or settings here if needed.
+    -- Example: OmniSharp specific command for restarting the server
+    -- This is often useful if OmniSharp gets into a weird state.
+    vim.keymap.set("n", "<leader>oR", function()
+      vim.cmd.OmniSharpRestartServer()
+      print("OmniSharp server restarted.")
+    end, { buffer = bufnr, noremap = true, silent = true, desc = "Restart OmniSharp" })
+  end,
+  capabilities = capabilities,
+  -- The cmd might be automatically handled if you use mason-lspconfig.
+  -- If omnisharp is in your PATH, this (or lspconfig's default) should work.
+  -- Ensure 'omnisharp' executable (or omnisharp.sh script) is found.
+  cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.v.pproccessid or vim.fn.getpid()) },
+  filetypes = { "cs", "vb" }, -- C# and VB.NET
+  root_dir = csharp_root_dir,
+  -- Enable modern .NET features. These are often defaults in newer omnisharp-roslyn but explicit can be good.
+  enable_roslyn_analyzers = true,
+  organize_imports_on_format = true,
+  enable_import_completion = true,
+  sdk_include_prereleases = true, -- If you use .NET preview SDKs
+
+  -- If you have an omnisharp.json in your project root, OmniSharp will pick up settings from there.
+  -- For example, to specify a target .NET SDK version or formatting options:
+  -- {
+  --   "FormattingOptions": {
+  --     "EnableEditorConfigSupport": true, // Recommended
+  --     "OrganizeImports": true
+  --   }
+  -- }
 }
